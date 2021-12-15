e06659637e7eb0099032e22c6de4711d90ff527f
Prathima0708 <kotianpriya64@gmail.com>
2021-12-15 11:23:24 +0530

    js files

---
+++ script.js
@@ -0,0 +1,64 @@
+// var a ,b ,add,sub,mul,div,x,y,c;
+//       a=50 ,b=10;
+//       x="hi";
+//       y="hello";
+//       c=x + " " + y;
+//       add=a+b;
+      
+//       sub=a-b;
+//       mul=a*b;
+//       div=a/b;
+//       console.log(add);
+//       console.log(sub);
+//       console.log(mul);
+//       console.log(div);
+//       console.log(c);
+   
+//       function myFunction(p1,p2)
+//       {
+//         var f;
+//         return p1*p2;
+//       }
+//      //  console.log(myFunction(3,4));
+//    f=myFunction(3,4);
+//    console.log(f);
+// let a=10;
+// if(a<5)
+// {
+//     console.log("a is less than 5");
+
+// }
+// else if(a<20 && a==10 ){
+//     console.log("a is equal to 10");
+    
+// }
+// else
+// {
+//     console.log("exit");
+// }
+// while(a<20)
+// {
+//     console.log(a);
+//     a++;
+// }
+// do{
+//     console.log(a);
+//     a++;
+// }while(a<=20)
+// let i;
+// for (i=0;i<10;i++)
+// {
+//     console.log(i);
+// }
+// console.log("exit");
+
+var b=10;
+switch(b)
+{
+    case 10:console.log("a is 10");
+    break;
+    case 20:console.log("a is 20");
+    break;
+    default :console.log("exit");
+}
+console.log("end");
